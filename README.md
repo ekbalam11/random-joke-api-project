6dfd64bf721e34bec9227006c43a0312e6ac04b2
ekbalam11 <ekbalam11@gmail.com>
2024-05-23 20:13:09 +0200

    add jokes category menu

--- README.md
+++ README.md
@@ -20,9 +20,12 @@ lado cliente al servidor.
 2. Implementa el mismo comportamiento de la demo, tal que cada vez que hagas clic en el botón __Get Joke!__ recupere un chiste aleatorio de la API https://api.chucknorris.io/jokes/random y lo muestre en el elemento del DOM adecuado
 
 ## Bonus
-
+<!-- 
 1. Crea un selector de manera que puedas escoger la categoría del chiste. Revisa la documentación oficial y descubrirás que pueden invocar a la API con un parámetro para que te de un _chiste_ de una categoría en concreto
-2. Investiga como podrias hacerlo para que, en el caso que ocurriera algún error en la llamada AJAX, se mostrará un mensaje error en el mismo contenedor; informando al usuario que algo ha ido mal. Puedes simular dicho comportamiento por ejemplo, cambiando la URL de la API a una dirección que no exista.
+2. Investiga como podrias hacerlo para que, en el caso que ocurriera algún error en la llamada AJAX, se mostrará un mensaje error en el mismo contenedor; informando al usuario que algo ha ido mal. Puedes simular dicho comportamiento por ejemplo, cambiando la URL de la API a una dirección que no exista. -->
+
+Crea un selector de manera que puedas escoger la categoría del chiste. Revisa la documentación oficial y descubrirás que pueden invocar a la API con un parámetro para que te de un chiste de una categoría en concreto. Haz clic aquí para ver una demo, pero revisa también como tienes que conformar la URL de la petición para obtener el chiste en función de la categoría.
+
 
 ## Otras soluciones
 
--- app.js
+++ app.js
@@ -3,13 +3,57 @@ const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
 
-button.addEventListener('click', async function() {
-    //1. Hacer una petición GET a la API que nos ofrece el chiste de Chuck Norris. Existe un método nativo de JS que se llama "fetch.". En su verstión más simple, simplemente la tenemos que pasar como parámetro, la URL de la API que queremos consumir. El método fetch devuelve la respuesta de la API.
-    const response = await fetch("https://api.chucknorris.io/jokes/random");
-    console.log(response);
-    //2. Convertir el JSON que nos devuelve la API a un tipo de dato que entienda JS. Típicamente será un objeto o un array de objetos).
+
+
+button.addEventListener('click', async function () {
+
+    // 1. Hacer una petición GET a la API que nos ofrece el chiste de Chuck Norris. Existe un método nativo de JavaScript que se llama 'fetch'. En su versión más simple, simplemente le tenemos que pasar como parámetro, la URL de la API que queremos consumir. El método fetch devuelve la respuesta de la API
+
+    var x = document.getElementById("mySelect");
+    var option = document.createElement("option");
+    option.text = "Kiwi";
+    option.value = "kiwi";
+    x.add(option);    
+
+    const categoryJoke = document.querySelector('#category'); 
+
+    let url = "https://api.chucknorris.io/jokes/random";
+        if (categoryJoke.value == "") {
+                url += "?category=" + categoryJoke.value;
+        }
+
+        console.log("esta es la url: ", url);
+
+    const response = await fetch(url);
+    console.dir("objeto response", response);
+
+        
+
+
+  
+    // 2. Modificar la URL para que ahora tenga en cuenta la category. Tenéis que obtener el valor del <select> que ha seleccionado el usuario. Por ejemplo, si ha seleccionado "food", debes construir la query string category=food
+
+    
+
     const data = await response.json();
-    //3. Modificar el DOM para mostrar el chiste al usuario.
+    console.log("objeto data", data);
+
+
+    // // 3. Modificar el DOM para mostrar el chiste al usuario
     jokeDIV.textContent = data.value;
 
 });
+
+// Errores comunes
+
+// 1. No poner bien la URL de la APIs de la cual quieres recibir la información
+//  await fetch("https://api.chucknorris.io/jokes/random");
+
+// 2. No saber interpretar el tipo de dato que os viene de la API
+// La forma más fácil de interpertar la estructura de datos que te devuelve la API-->
+// 1. console.log(data) --> Mostrar por consola los datos de la API antes de hacer nada
+// 2. En la página Web de la API, existe documentación con los campos y estructura de datos que devuelve la API
+
+// 3. No usar el ejercicio de chuck norris como base. Esta bien mirar como se hace el fetch en otros ejercicios. No copiar y pegar, escribirlo todo para que se vaya quedando e interorizando
+
+
